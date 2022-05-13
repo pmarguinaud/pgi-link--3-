872b0d5c5018764c89154de340f04fb37ad3beb2
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-05-13 17:07:33 +0000

    Prune

--- cmd.sh
+++ cmd.sh
@@ -10,9 +10,5 @@ pgf90 -c -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio -Mbackslash -O0 -
 
 ar crv 'lib[3].a' type_toto.o
 
-pgf90 \
-  -acc=gpu -ta=tesla \
-  -Mcuda -traceback -Mbyteswapio \
-  -Wl,--start-group ./test_load_model.o -L. -l[3] -Wl,--end-group 
-
-pgf90 -o toto.x -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio test_load_model.o type_toto.o 
+pgf90 -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio -Wl,--start-group ./test_load_model.o -L. -l[3] -Wl,--end-group 
+pgf90 -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio test_load_model.o type_toto.o -o toto.x
