79b75a20f3e3cf474e2f3eabc186cac739e2431a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-05-13 17:10:02 +0000

    Prune

--- cmd.sh
+++ cmd.sh
@@ -8,7 +8,9 @@ set -x
 pgf90 -c -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio -Mbackslash -O0 -g -acc=gpu -ta=tesla -Mcuda type_toto.F90
 pgf90 -c -Mlarge_arrays -Mlist -traceback -Mnofma -Mbyteswapio -Mbackslash -O0 -g -acc=gpu -ta=tesla -Mcuda  test_load_model.F90
 
-ar crv 'lib[3].a' type_toto.o
+LIB="[3]"
+
+ar crv "lib$LIB.a" type_toto.o
 
 pgf90 -o toto1.x -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio                   test_load_model.o type_toto.o 
-pgf90 -o toto2.x -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio -Wl,--start-group test_load_model.o -L. -l[3] -Wl,--end-group 
+pgf90 -o toto2.x -acc=gpu -ta=tesla -Mcuda -traceback -Mbyteswapio -Wl,--start-group test_load_model.o -L. -l$LIB -Wl,--end-group 
