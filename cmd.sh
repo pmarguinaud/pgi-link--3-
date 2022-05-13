3c85f19da0a793ca918dfc2bba61edf9181ada10
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-05-13 16:59:22 +0000

    Prune

--- cmd.sh
+++ cmd.sh
@@ -19,12 +19,6 @@ export PATH=/opt/softs/nvidia/hpc_sdk/Linux_x86_64/21.7/compilers/bin:/opt/softs
 /opt/softs/nvidia/hpc_sdk/Linux_x86_64/21.7/comm_libs/mpi/bin/mpif90 \
   -acc=gpu -ta=tesla:lineinfo \
   -Mcuda -traceback -Mbyteswapio \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/nvshmem/lib \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/nccl/lib \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/comm_libs/mpi/lib \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/math_libs/lib64 \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/compilers/lib \
-  -Wl,-rpath,/opt/nvidia/hpc_sdk/Linux_x86_64/20.9/cuda/lib64 \
   -Wl,--start-group ./test_load_model.o -L. -l[3] -Wl,--end-group \
   -L/opt/softs/nvidia/hpc_sdk/Linux_x86_64/21.7/compilers/lib -lnvhpcatm \
   -Wl,-rpath,/opt/softs/nvidia/hpc_sdk/Linux_x86_64/21.7/compilers/lib
